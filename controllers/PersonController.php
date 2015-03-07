a2e34ea976d0eb8aefe963eec17110bea5fcc45d
Manop Kongoon <kongoon@hotmail.com>
2015-03-07 17:20:01 +0700

    อัพเดทไฟล์

--- controllers/PersonController.php
+++ controllers/PersonController.php
@@ -1,12 +1,25 @@
 <?php
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
 use app\models\PersonForm;
 
 class PersonController extends Controller{
+    public function actions(){
+        return [
+            'captcha'=>[
+                'class'=>'yii\captcha\CaptchaAction',
+            ],
+        ];
+    }
     public function actionTest1(){
         $model = new PersonForm(['scenario'=>'s2']);
-        return $this->render('test1',['model'=>$model]);
+        if($model->load(Yii::$app->request->post())){
+            $value = $_POST['PersonForm'];
+        }else{
+            $value = null;
+        }
+        return $this->render('test1',['model'=>$model,'value'=>$value]);
     }
 }
--- views/person/test1.php
+++ views/person/test1.php
@@ -4,15 +4,17 @@ use yii\widgets\ActiveForm;
 use yii\captcha\Captcha;
 
 ?>
-
+<h1>เพิ่ม Person</h1>
 <?php $form = ActiveForm::begin();?>
 <?= $form->field($model,'firstname');?>
 <?= $form->field($model,'lastname');?>
 <?= $form->field($model,'email');?>
 <?= $form->field($model,'address')->textarea(['rows'=>5]);?>
 <?= $form->field($model,'verifyCode')->widget(Captcha::className(),[
-    
+    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
 ]);?>
 <?= Html::submitButton('บันทึก',['class'=>'btn btn-primary']);?>
 
 <?php ActiveForm::end();?>
+
+<?php print_r($value);?>
